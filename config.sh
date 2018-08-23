7f4fe12ebaeec8db99413a8892cd7a56e1f2c7c5
beyondrya <who112445778@qq.com>
2018-08-23 16:22:43 +0800

    ip auto config

--- config.sh
+++ config.sh
@@ -3,6 +3,9 @@ service mysql start
 mysql < ./schema.sql 
 service mysql status 
 
+myip=$(curl ipconfig.me)
+sed -i "s/<externalAddress>.*<\/externalAddress>/<externalAddress>$myip<\/externalAddress>/g" ./res/server/kbengine.xml 
+
 sh ./start_server.sh
 sh -c "$*" 
 
